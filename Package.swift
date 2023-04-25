fecdd733898052a6105c9c07efac88d03ec75edc
imazio <imazio@gmail.com>
2023-04-25 17:06:32 +0900

    Version 0.0.2

--- Package.swift
+++ Package.swift
@@ -3,8 +3,35 @@
 
 import PackageDescription
 
+//let package = Package(
+//    name: "CustomFramework",
+//    products: [
+//        // Products define the executables and libraries a package produces, and make them visible to other packages.
+//        .library(
+//            name: "CustomFramework",
+//            targets: ["CustomFramework"]),
+//    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        // .package(url: /* package url */, from: "1.0.0"),
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "CustomFramework",
+//            dependencies: []),
+//        .testTarget(
+//            name: "CustomFrameworkTests",
+//            dependencies: ["CustomFramework"]),
+//    ]
+//)
+
 let package = Package(
     name: "CustomFramework",
+    platforms: [
+      .macOS(.v12), .iOS(.v16 )
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +45,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "CustomFramework",
-            dependencies: []),
-        .testTarget(
-            name: "CustomFrameworkTests",
-            dependencies: ["CustomFramework"]),
+            path: "./Sources/CustomFramework.xcframework")
     ]
 )
