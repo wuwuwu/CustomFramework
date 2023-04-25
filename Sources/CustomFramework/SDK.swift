1881548600886072161cf9bee4293cb3a20f7818
imazio <imazio@gmail.com>
2023-04-25 15:41:28 +0900

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by imazio on 25/04/2023.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork(){
+        print("some work...")
+    }
+}
